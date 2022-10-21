0d7a037577fb653d657ee92af0cf65e9749eea0e
Writiger <75297838+writiger@users.noreply.github.com>
2022-10-21 14:52:06 +0800

    添加redis模块

--- server/app/domain/DB.go
+++ server/app/domain/DB.go
@@ -4,14 +4,35 @@ import (
 	"fmt"
 	u "server/utils"
 
+	"github.com/go-redis/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"xorm.io/xorm"
 )
 
+// 使用xorm映射数据库
 var engine *xorm.Engine
 
+// 声明一个全局的redisDb变量
+var redisDb *redis.Client
+
+func InitRedisClient() *redis.Client {
+	c := u.GetConf()
+	// 获取redisClient
+	redisDb = redis.NewClient(&redis.Options{
+		Addr:     c.RedisSourceName,
+		Password: c.RedisPassword,
+		DB:       c.RedisDB,
+	})
+	_, err := redisDb.Ping().Result()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("链接Redis成功")
+	return redisDb
+}
+
 // 初始化数据库
-func InitDB() *xorm.Engine {
+func InitSqlDB() *xorm.Engine {
 	c := u.GetConf()
 	var err error
 	engine, err = xorm.NewEngine("mysql", c.DataSourceName)
