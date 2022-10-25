2b77f75de7fc7ea6b3acded3466119db86af750b
Writiger <75297838+writiger@users.noreply.github.com>
2022-10-25 18:57:37 +0800

    Update MemoApi.go

--- server/app/api/MemoApi.go
+++ server/app/api/MemoApi.go
@@ -3,17 +3,25 @@ package api
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"server/app/service"
+	"server/response"
 )
 
 func MemoQueryByOwner(ctx *gin.Context) {
 	// 1. 读取用户id
-	owner := ctx.Param("owner")
+	ownerAny, _ := ctx.Get("userId")
+	owner := fmt.Sprintf("%v", ownerAny)
 	// 2. 调用service
 	res, err := service.MemoQueryByOwnerService(owner)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	fmt.Println(res)
 	// 3. 响应请求
+	if err != nil {
+		ctx.JSON(http.StatusOK, response.Err.WithMsg(err.Error()))
+		return
+	}
+	ctx.JSON(http.StatusOK, response.OK.WithData(res))
+}
+
+func MemoUpload(ctx *gin.Context) {
+	fmt.Println(ctx.Get("userId"))
 }
