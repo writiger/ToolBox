f660482f58dceeed98c0330aaa07f62626829236
Writiger <75297838+writiger@users.noreply.github.com>
2022-10-29 13:13:24 +0800

    优化返回值

--- server/rsa/rsa.go
+++ server/rsa/rsa.go
@@ -7,7 +7,7 @@ import (
 	"encoding/base64"
 )
 
-// MakeKeys 生成基于X509加密的私钥和公钥
+// MakeKeys 生成基于X509加密Base64编码的私钥和公钥的字符串
 func MakeKeys() (X509PrivateKey string, X509PublicKey string) {
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
@@ -22,37 +22,42 @@ func MakeKeys() (X509PrivateKey string, X509PublicKey string) {
 	return
 }
 
-// Encrypt
+// Encrypt 加密
 // @Param plain string 需要加密的信息
-// @Param publicByte []byte 公钥
-func Encrypt(plain string, publicK string) ([]byte, error) {
+// @Param publicByte string 公钥
+// @return encryptedPlain string 加密后的密文
+func Encrypt(plain string, publicK string) (string, error) {
 	// Base64解码
 	keyGet, _ := base64.StdEncoding.DecodeString(publicK)
 	// X509解码
 	publicKeyInterface, err := x509.ParsePKIXPublicKey(keyGet)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	//类型断言
 	publicKey := publicKeyInterface.(*rsa.PublicKey)
 	encryptedBytes, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, []byte(plain))
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return encryptedBytes, nil
+	encryptedPlain := base64.StdEncoding.EncodeToString(encryptedBytes)
+	return encryptedPlain, nil
 }
 
-// Decrypt
-// @param
-func Decrypt(cipher []byte, privateK string) ([]byte, error) {
+// Decrypt 解密
+// @param cipher string 加密后的密文
+// @param privateK string base64编码的私钥
+// @return plainText string 解密后的明文
+func Decrypt(cipher string, privateK string) (string, error) {
+	cipherByte, _ := base64.StdEncoding.DecodeString(cipher)
 	// Base64解码
 	keyGet, _ := base64.StdEncoding.DecodeString(privateK)
 	//X509解码
 	privateKey, err := x509.ParsePKCS1PrivateKey(keyGet)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
 	//对密文进行解密
-	plainText, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipher)
-	return plainText, nil
+	plainText, _ := rsa.DecryptPKCS1v15(rand.Reader, privateKey, cipherByte)
+	return string(plainText), nil
 }
