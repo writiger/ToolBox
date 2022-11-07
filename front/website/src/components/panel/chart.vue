06ff19eba2dbdcb39995e1d66b8ca744bd19e6c8
Writiger <75297838+writiger@users.noreply.github.com>
2022-11-07 13:49:14 +0800

    Panel框架

---
+++ front/website/src/components/panel/chart.vue
@@ -0,0 +1,77 @@
+<template>
+    <n-card>
+        <h3>最近30天打卡情况</h3>
+        <v-chart class="chart" :option="option" />
+    </n-card>
+</template>
+  
+<script>
+import { NCard } from 'naive-ui'
+import { use } from 'echarts/core';
+import { GridComponent } from 'echarts/components';
+import { LineChart } from 'echarts/charts';
+import { UniversalTransition } from 'echarts/features';
+import { CanvasRenderer } from 'echarts/renderers';
+import VChart, { THEME_KEY } from 'vue-echarts';
+import { reactive, defineComponent } from 'vue';
+
+
+use([
+    GridComponent,
+    LineChart,
+    CanvasRenderer,
+    UniversalTransition
+]);
+const xData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+const option = reactive({
+    title: {
+        text: '最近30天打卡情况',
+        left: '1%'
+    },
+    animationDuration: 5000,
+    xAxis: {
+        type: 'category',
+        data: xData
+    },
+    yAxis: {
+        type: 'value'
+    },
+    series: [
+        {
+            data: [150, 230, 224, 218, 135, 147, 260],
+            type: 'line'
+        }
+    ]
+});
+export default defineComponent({
+    name: 'HelloWorld',
+    components: {
+        VChart,
+        NCard
+    },
+    provide: {
+        [THEME_KEY]: 'light',
+    },
+    mounted() {
+        for (var i = 0; i < 30; i++) {
+            option.series[0].data[i] = i
+        }
+    },
+    setup() {
+        return { option };
+    },
+});
+</script>
+  
+<style scoped>
+.chart {
+    width: 100%;
+}
+
+.n-card {
+    margin-top: -10px;
+    height: 250px;
+}
+</style>
+  
--- front/website/src/components/panel/index.vue
+++ front/website/src/components/panel/index.vue
@@ -1,17 +1,96 @@
 <template>
-    <div>
-
+    <div class="panel">
+        <n-grid x-gap="48" :cols="3">
+            <n-grid-item>
+                <n-card class="firstThree" title="打卡" hoverable>
+                    卡片内容
+                </n-card>
+            </n-grid-item>
+            <n-grid-item>
+                <n-card class="firstThree" title="查询密码" hoverable>
+                    卡片内容
+                </n-card>
+            </n-grid-item>
+            <n-grid-item>
+                <n-card class="firstThree" title="番茄钟" hoverable>
+                    卡片内容
+                </n-card>
+            </n-grid-item>
+        </n-grid>
+        <br>
+        <n-grid x-gap="48" :cols="4">
+            <n-grid-item :span="1">
+                <n-card class="calendar" title="日程" hoverable>
+                    卡片内容
+                </n-card>
+            </n-grid-item>
+            <n-grid-item :span="3">
+                <n-carousel effect="card" prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
+                    next-slide-style="transform: translateX(50%) translateZ(-800px);" style="height: 240px"
+                    :show-dots="false">
+                    <n-carousel-item :style="{ width: '70%' }">
+                        <img class="carousel-img"
+                            src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg">
+                    </n-carousel-item>
+                    <n-carousel-item :style="{ width: '70%' }">
+                        <img class="carousel-img"
+                            src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg">
+                    </n-carousel-item>
+                    <n-carousel-item :style="{ width: '70%' }">
+                        <img class="carousel-img"
+                            src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg">
+                    </n-carousel-item>
+                    <n-carousel-item :style="{ width: '70%' }">
+                        <img class="carousel-img"
+                            src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg">
+                    </n-carousel-item>
+                </n-carousel>
+                <chart></chart>
+            </n-grid-item>
+        </n-grid>
     </div>
 </template>
 
 <style scoped>
+.panel {
+    margin: 40px 68px 0 68px;
+}
 
+.firstThree {
+    height: 150px;
+}
+
+.carousel-img {
+    margin: 0 auto;
+    width: 100%;
+    height: 80%;
+    object-fit: cover;
+}
+
+.calendar {
+    min-height: 500px;
+}
 </style>
 
 <script lang='ts'>
+import {
+    NGrid,
+    NGridItem,
+    NCard,
+    NCarousel,
+    NCarouselItem,
+} from 'naive-ui'
+import chart from './chart.vue'
 import { Options, Vue } from "vue-class-component"
 @Options({
-
+    components: {
+        NGrid,
+        NGridItem,
+        NCard,
+        NCarousel,
+        NCarouselItem,
+        chart
+    }
 })
 export default class Panel extends Vue {
 }
