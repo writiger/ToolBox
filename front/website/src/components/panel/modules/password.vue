d6827d24aba81e8a1a8c4d6b004a83cf296c6db3
Writiger <75297838+writiger@users.noreply.github.com>
2022-11-13 15:01:09 +0800

    webDemo 0.0.Beta1

--- front/website/src/components/panel/modules/password.vue
+++ front/website/src/components/panel/modules/password.vue
@@ -3,8 +3,21 @@
         <n-grid :cols="5">
             <n-grid-item :span="2">
                 <n-dropdown trigger="hover" placement="bottom-start" :options="op" @select="handleSelect">
-                    <n-button> 你要查询的密码用途 </n-button>
+                    <n-button size="tiny"> 你要查询的密码用途 </n-button>
                 </n-dropdown>
+                <br>
+                <br>
+                <n-button size="tiny" @click="showModal = true">添加</n-button>
+                <n-modal v-model:show="showModal">
+                    <n-card title="保存密码" style="width:600px;text-align: center;">
+                        <n-space vertical>
+                            <n-input v-model:value="newpwd1" placeholder="输入你的密码"></n-input>
+                            <n-input v-model:value="newpwd2" placeholder="再次输入你的密码"></n-input>
+                            <n-input v-model:value="newpwdFor" placeholder="输入这个密码的用途"></n-input>
+                            <n-button size="tiny">上传</n-button>
+                        </n-space>
+                    </n-card>
+                </n-modal>
             </n-grid-item>
             <n-grid-item :span="3">
                 <n-input class="input" v-model:value="value" round type="textarea" placeholder="输入你的密钥" size="small" />
@@ -26,7 +39,10 @@ import {
     NGridItem,
     NInput,
     NButton,
-    NDropdown
+    NDropdown,
+    NModal,
+    NCard,
+    NSpace
 } from "naive-ui"
 @Options({
     components: {
@@ -34,11 +50,18 @@ import {
         NGridItem,
         NInput,
         NButton,
-        NDropdown
+        NModal,
+        NDropdown,
+        NCard,
+        NSpace
     }
 })
 export default class password extends Vue {
     value = ""
+    showModal = false
+    newpwd1 = ""
+    newpwd2 = ""
+    newpwdFor = ""
     op = [
         {
             label: '滨海湾金沙，新加坡',
